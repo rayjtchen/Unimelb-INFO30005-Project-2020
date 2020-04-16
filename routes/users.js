461179454d75c4c39c16aea8f49b0d23e6639281
yssheng <61829224+yssheng@users.noreply.github.com>
2020-04-16 11:24:54 +1000

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+var users_controller = require('../controllers/users_controller');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// register form
+router.get('/register', function(req, res){
+  res.render('register');
+});
+
+// login form
+router.get('/login', function(req, res){
+  res.render('login');
+});
+
+router.post('/register', users_controller.registerNewUser);
 
 module.exports = router;
