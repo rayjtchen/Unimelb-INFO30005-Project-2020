76f3bdc2d32a8e0768b8af4d1841f16341cacde3
yssheng <61829224+yssheng@users.noreply.github.com>
2020-04-17 15:50:16 +1000

    spelling mistakes fixed

--- controllers/validator.js
+++ controllers/validator.js
@@ -2,11 +2,11 @@ const { check } =  require('express-validator');
 
 exports.createUser  = [
     check('email', 'Email is required').not().isEmpty(),
-    check('email', 'Email is not vaild').isEmail(),
+    check('email', 'Email is not valid').isEmail(),
     check('username', 'Username is required').not().isEmpty(),
     check('password', 'Password is required').not().isEmpty(),
     check('password2', 'Passwords do not match').exists().custom((value,{req})=>{
-        return value == req.body.password;
+        return value === req.body.password;
     })
 ];
 
