46e05cf08f0569f2b80b87e28cf006073565a185
Ray Chen <46772434+rayjtchen@users.noreply.github.com>
2020-04-19 00:17:20 +1000

    add 'Email is already registered' error
    add 'Email is already registered' error in validator.js

--- controllers/validator.js
+++ controllers/validator.js
@@ -1,4 +1,5 @@
 const { check } =  require('express-validator');
+const user = require('../models/user');
 
 exports.createUser  = [
     check('email', 'Email is required').not().isEmpty(),
@@ -7,6 +8,34 @@ exports.createUser  = [
     check('password', 'Password is required').not().isEmpty(),
     check('password2', 'Passwords do not match').exists().custom((value,{req})=>{
         return value === req.body.password;
+    }),
+    check('email', 'Email is already registered').exists().custom((value)=>
+    {
+        user.findOne({email: value})
+            .then(()=>{return false;})
+            .catch(()=>{return true;});
     })
 ];
 
+/*
+var validationCheck = function()
+{
+    return exports.createUser  = [
+        check('email', 'Email is required').not().isEmpty(),
+        check('email', 'Email is not valid').isEmail(),
+        check('username', 'Username is required').not().isEmpty(),
+        check('password', 'Password is required').not().isEmpty(),
+        check('password2', 'Passwords do not match').exists().custom((value,{req})=>{
+            return value === req.body.password;
+        }),
+        check('email', 'Email is already registered').exists().custom((value)=>
+        {
+            user.findOne({email: value})
+                .then(()=>{return false;})
+                .catch(()=>{return true;});
+        })
+    ];
+}
+
+module.exports.validationCheck = validationCheck;
+*/
