e51bcce542ad8cb20d8ae6d6a52bfaec9d5235e7
yssheng <yssheng@student.unimelb.edu.au>
2020-05-28 21:37:41 +1000

    About us added
    added a new route called 'About Us' to briefly describe the website

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,9 @@ router.get('/', function(req, res, next)
   res.render('index', { title: 'UMSV' });
 });
 
+router.get('/aboutus', function(req, res, next)
+{
+  res.render('aboutus', { title: 'UMSV' });
+});
+
 module.exports = router;
