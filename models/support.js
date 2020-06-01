3a86419b6ece2d608977e95c33218bd735800327
Ray Chen <46772434+rayjtchen@users.noreply.github.com>
2020-06-01 17:34:42 +1000

    move image slider to articles.pug for blog slogan
    move image slider to articles.pug for blog slogan

---
+++ models/support.js
@@ -0,0 +1,29 @@
+let mongoose = require('mongoose');
+var ObjectId = require('mongodb').ObjectID;
+
+
+//Support Schema
+let supportSchema = mongoose.Schema({
+    article_id:{
+        type: ObjectId,
+        require: true
+    },
+    user_id:{
+        type: ObjectId,
+        require: true
+    },
+    user_name:{
+        type: String,
+        required: true
+    },
+    user_email:{
+        type: String,
+        require: true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+let Support = module.exports = mongoose.model('Support', commentSchema);
